cb8e1973e386bb6d0df026c0d69b5cbd04b013a4
Talwarh1010 <100240731+Talwarh1010@users.noreply.github.com>
2024-02-09 10:57:41 +1300

    Add files via upload
    Updated the rounds class buttons into a loop

---
+++ 02_rounds_play_v03.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+
+class ChooseRounds:
+
+    def __init__(self):
+        button_font = ("Arial", "13", "bold")
+        button_fg = "#FFFFFF"
+        self.intro_frame = Frame(padx=10, pady=10)
+        self.intro_frame.grid()
+        self.colour_frame = Frame(self.intro_frame)
+        self.colour_frame.grid(row=2)
+        self.colour_heading = Label(self.intro_frame,
+                                    text="Colour Quest",
+                                    font=("Arial", "16", "bold")
+                                    )
+        self.colour_heading.grid(row=0)
+
+        instructions = "In each round you will be given six different colours to choose from. Pick a colour" \
+                       " and see if you can beat the computer's score!" \
+                       " \nTo begin, choose how many rounds you'd like to play..."
+        self.display_instructions = Label(self.intro_frame,
+                                          text=instructions,
+
+                                          wraplength=300,
+                                          justify="left")
+
+        self.display_instructions.grid(row=1)
+
+        btn_colour_value = [["#CC0000", 3], ["#CC0000", 5], ["#CC0000", 10]]
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Colour Quest")
+    ChooseRounds()
+    root.mainloop()
